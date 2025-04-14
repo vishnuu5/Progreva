c6a062dfbbb5bac2b72b79a8e652704debce23ab
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-04-14 11:12:37 +0530

    FeaturesSection.jsx

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -19,10 +19,10 @@ export default function FeaturesSection() {
     ]
 
     return (
-        <div className="mt-6 grid grid-cols-2 gap-[7rem] md:grid-cols-4">
+        <div className="mt-6 grid grid-cols-2 gap-[3rem] md:grid-cols-4">
             {features.map((feature, index) => (
                 <div key={index} className="flex items-center bg-lime-800 rounded-xl w-[9rem] py-2 ">
-                    <span className="text-2xl ">{feature.icon}</span>
+                    <span className="text-2xl pl-2">{feature.icon}</span>
                     <span className="text-xs text-white ">{feature.title}</span>
                 </div>
             ))}
